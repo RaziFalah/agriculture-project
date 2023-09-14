b75c091b0d718be540a88ebb04539fd685a589ca
Razi Falah <i@razifalah.com>
2023-09-14 09:27:29 +0300

    Update Script1.cpp

--- code/Script1.cpp
+++ code/Script1.cpp
@@ -1,1 +1,12 @@
+//No additional script required.
 
+void setup() {
+  pinMode(12, OUTPUT); //initialise pin 12 and assign it as output 
+}
+
+void loop() {
+  digitalWrite(12, HIGH); 
+  delay(100);                 
+  digitalWrite(12, LOW);   
+  delay(100);                       
+}
